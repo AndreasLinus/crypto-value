4f71e5fc75e435204f64aa45a96dcf1bb97edc3f
Andreas Filipsson <andreas.filipsson@regionstockholm.se>
2025-02-13 10:37:39 +0100

    Removed TODO

--- app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/data/remote/AppApi.kt
+++ app/src/main/java/com/ferhatozcelik/jetpackcomposetemplate/data/remote/AppApi.kt
@@ -17,16 +17,11 @@ interface AppApi {
 
     companion object {
         const val BASE_PATH = "https://api.wazirx.com/sapi/v1/tickers/24hr"
-        const val BASE_PATH_CURRENCY = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/usd.json"
+        const val BASE_PATH_CURRENCY =
+            "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/usd.json"
         //const val BASE_PATH = "api.wazirx.com/sapi/v1/tickers/{hours}"
     }
 
-    // TODO: add query parameter for hours to fetch
-    /*@Headers("Accept: application/json")
-    @GET(BASE_PATH)
-    suspend fun getExampleResult(
-        @Query("hour") hours: String
-    ): Response<List<CryptoData>>*/
 }
 
 
